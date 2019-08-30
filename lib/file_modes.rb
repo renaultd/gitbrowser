b59ff9c925f59596be90a46014db9fae0838f364
David Renault <renault@enseirb.fr>
2019-08-30 18:05:03 +0200

    Add some more interesting file modes

--- lib/file_modes.rb
+++ lib/file_modes.rb
@@ -1,14 +1,16 @@
 module FileModes
 
   MODES = {
-    "c"    => "ace/mode/c_cpp",
-    "cpp"  => "ace/mode/c_cpp",
-    "h"    => "ace/mode/c_cpp",
-    "hpp"  => "ace/mode/c_cpp",
-    "html" => "ace/mode/html",
-    "py"   => "ace/mode/python",
-    "rb"   => "ace/mode/ruby",
-    "rkt"  => "ace/mode/scheme",
+    "c"        => "ace/mode/c_cpp",
+    "cpp"      => "ace/mode/c_cpp",
+    "h"        => "ace/mode/c_cpp",
+    "hpp"      => "ace/mode/c_cpp",
+    "html"     => "ace/mode/html",
+    "js"       => "ace/mode/javascript",
+    "Makefile" => "ace/mode/makefile",
+    "py"       => "ace/mode/python",
+    "rb"       => "ace/mode/ruby",
+    "rkt"      => "ace/mode/scheme",
   }
 
   DEFAULT_MODE = "ace/mode/text"
